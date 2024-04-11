adcd1743fa9ef301cfcdd1a98f1ff5f817afcd37
Dharaneesh S <dharaneeshdharaneesh76@gmail.com>
2024-04-11 07:22:11 +0530

    Final code

---
+++ webpack.prod.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const { optimize } = require("webpack");
+
+module.exports = {
+  entry: {
+    main: "./index.js",
+  },
+  output: {
+    path: path.join(__dirname, "prod-build-optimization"),
+    publicPath: "/",
+    filename: "[name].js",
+    clean: true,
+  },
+  mode: "production",
+    target: "node",
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                compress: {
+                    drop_console:true
+                }
+            }
+        })]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
